e803853c915a40a6833261927424b4bc946c3bcb
ZZZ <529520158@qq.com>
2025-12-16 21:33:59 +0800

    fix naming

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-import { lustitana } from './ui/fonts';
+import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
@@ -17,7 +17,7 @@ export default function Page() {
         <div
   className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
 />
-          <p className={`${lustitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,7 @@ import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
-export const lustitana = Lusitana({
+export const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
 });
