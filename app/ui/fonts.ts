3390e5c978506450ccf3c093b52b7b57e6d8cb83
ZZZ <529520158@qq.com>
2025-12-16 21:04:46 +0800

    WIP

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lustitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
